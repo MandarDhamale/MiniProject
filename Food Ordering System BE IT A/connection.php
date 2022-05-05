c1bc74520aeb4cb965a407c23ffd5c0aeca3255b
Mandar Dhamale <57679052+MandarDhamale@users.noreply.github.com>
2022-05-05 12:31:06 +0530

    Add files via upload

---
+++ Food Ordering System BE IT A/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$link = mysqli_connect("localhost", "id15659092_prajwal_26", "PrajwalPatki@9703771284", "id15659092_bon_appetit");
+        
+        if (mysqli_connect_error())
+        {
+         die ("Database Connection Error");   
+        }
+        
+        
+?>
